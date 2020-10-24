8760008237db3437a99113af1718fa1bd25a4c91
volantsk <eugenio.fec@gmail.com>
2020-10-24 18:29:25 -0300

    Altera hello para programa teste avengers de estudo

--- go/hello.go
+++ go/hello.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+// Avenger represents a single hero
+type Avenger struct {
+	RealName string `json:"real_name"`
+	HeroName string `json:"hero_name"`
+	Planet   string `json:"planet"`
+	Alive    bool   `json:"alive"`
+}
+
+func add(x, y int) int {
+	return (x + y)
+}
+
+func (a *Avenger) isAlive() {
+	a.Alive = true
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+	avengers := []Avenger{
+		{
+			RealName: "Dr. Bruce Banner",
+			HeroName: "Hulk",
+			Planet:   "Earth",
+		},
+		{
+			RealName: "Tony Stark",
+			HeroName: "Iron Man",
+			Planet:   "Earth",
+		},
+		{
+			RealName: "Thor Odinson",
+			HeroName: "Thor",
+			Planet:   "Midgard",
+		},
+	}
+
+	avengers[1].isAlive()
+
+	jsonBytes, err := json.Marshal(avengers)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(string(jsonBytes))
 }
